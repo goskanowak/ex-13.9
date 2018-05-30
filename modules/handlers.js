a9c04e270352c9fb0d034abac165e9def14bca3c
Małgorzata Nowak <goskanowak87@gmail.com>
2018-05-30 14:14:54 +0200

    Add html and change handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 exports.upload = function(request, response) {
     console.log('Rozpoczynam obsługę żądania upload.'.green);
     response.write('Rozpoczynam upload');
@@ -6,8 +8,11 @@ exports.upload = function(request, response) {
 
 exports.welcome = function(request, response) {
     console.log('Rozpoczynam obsługę żądania welcome.'.green);
-    response.write('Witaj na stronie startowej');
-    response.end();
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        response.write(html);
+        response.end();
+    });
 }
 
 exports.error = function(request,response) {
